05613527bc0892fdb406598cd212c5a6ad6ab5b2
warishshahbaz <mdshahbazwarish1996@gmail.com>
2024-04-18 17:37:00 +0530

    init

--- src/components/App.js
+++ src/components/App.js
@@ -49,7 +49,7 @@ const App = () => {
       <button id="add-todo-btn" onClick={addTodo}>
         Add Todo
       </button>
-      <button id="incr-cnt" onClick={incrementCounter}>
+      <button id="incr-btn" onClick={incrementCounter}>
         {counter}
       </button>
       <UseMemo />
